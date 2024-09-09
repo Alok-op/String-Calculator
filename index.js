f28db65d1510d022d2a66c4c47c7ef2355aff679
Alok Jha <68098623+Alok-op@users.noreply.github.com>
2024-09-09 18:32:39 +0530

    handling negative number case

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ function add(number) {
   if (number === '') {
     console.log(0);
   }
+
+  if (number) {
+    // let num = number.replace(",", "");
+    let num = number.split(",");
+    let sum = 0;
+    for(let value of num) {
+      console.log(value)
+      if (value > 0) {
+        sum = sum + value;
+      } else {
+        console.log("negative numbers not allowed", value);
+      }
+    }
+    console.log("sum", sum)
+    // console.log(typeof(num))
+    console.log("num:", num)
+  }
   // let num = Number(number);
   // console.log(num);
 }
 
-add('');
+add('5');
