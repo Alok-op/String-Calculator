da64d867c75cbe6a967bb55bc36e1448c25205a2
Alok Jha <68098623+Alok-op@users.noreply.github.com>
2024-09-09 21:05:36 +0530

    handle different delimeter

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function add(number) {
     for (let value of num) {
       console.log(value);
       let val = Number(value);
-      if (val > 0) {
+      if (val < 0) {
+        negativeNumbers.push(val);
+      } else {
         sum = sum + val;
         console.log('sum', sum);
-      } else {
-        negativeNumbers.push(val);
         // console.log('negative numbers not allowed', val);
       }
     }
@@ -23,7 +23,38 @@ function add(number) {
     console.log('negative numbers not allowed', negativeNumbers.join(','));
     throw ('66negative numbers not allowed', negativeNumbers.join(','));
   }
+
+  if (number.startsWith('//')) {
+    console.log("kk")
+    const delimiterEndIndex = number.indexOf('\n');
+    console.log(delimiterEndIndex)
+    let delimiter = number.substring(2, delimiterEndIndex);
+    console.log("delimiter", delimiter)
+    if (delimiter.startsWith('[') && delimiter.endsWith(']')) {
+      delimiter = delimiter.slice(1, -1);
+    }
+    const numbersPart = number.substring(delimiterEndIndex + 1);
+    console.log("numbersPart", numbersPart)
+    const modifiedNumbers = numbersPart.split(delimiter).join(',');
+    console.log(modifiedNumbers)
+    let num = modifiedNumbers.split(/[,\n]/);
+    let sum = 0;
+    for (let value of num) {
+      console.log(value);
+      let val = Number(value);
+      if (val < 0) {
+        negativeNumbers.push(val);
+      } else {
+        sum = sum + val;
+        console.log('sum', sum);
+        // console.log('negative numbers not allowed', val);
+      }
+    }
+  }
 }
 
+
 // add('1,5');
-add('1\n2,3');
+// add('1\n2,3');
+// add('//[]\n1;[]2')
+add('//[***]\n1***3***3');
