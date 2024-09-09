2ba6c9c5ec264983ec7f4ebbbb9ce4552b1bd64b
Alok Jha <68098623+Alok-op@users.noreply.github.com>
2024-09-09 21:33:00 +0530

    Refactored code

--- index.js
+++ index.js
@@ -3,58 +3,46 @@ function add(number) {
     console.log(0);
   }
 
-  let negativeNumbers = [];
-  if (number) {
-    let num = number.split(/[,\n]/);
-    let sum = 0;
-    for (let value of num) {
-      console.log(value);
-      let val = Number(value);
-      if (val < 0) {
-        negativeNumbers.push(val);
-      } else {
-        sum = sum + val;
-        console.log('sum', sum);
-        // console.log('negative numbers not allowed', val);
-      }
-    }
-  }
-  if (negativeNumbers.length > 0) {
-    console.log('negative numbers not allowed', negativeNumbers.join(','));
-    throw ('66negative numbers not allowed', negativeNumbers.join(','));
-  }
-
   if (number.startsWith('//')) {
-    console.log("kk")
-    const delimiterEndIndex = number.indexOf('\n');
-    console.log(delimiterEndIndex)
-    let delimiter = number.substring(2, delimiterEndIndex);
-    console.log("delimiter", delimiter)
-    if (delimiter.startsWith('[') && delimiter.endsWith(']')) {
-      delimiter = delimiter.slice(1, -1);
-    }
-    const numbersPart = number.substring(delimiterEndIndex + 1);
-    console.log("numbersPart", numbersPart)
-    const modifiedNumbers = numbersPart.split(delimiter).join(',');
-    console.log(modifiedNumbers)
-    let num = modifiedNumbers.split(/[,\n]/);
-    let sum = 0;
-    for (let value of num) {
-      console.log(value);
-      let val = Number(value);
-      if (val < 0) {
-        negativeNumbers.push(val);
-      } else {
-        sum = sum + val;
-        console.log('sum', sum);
-        // console.log('negative numbers not allowed', val);
-      }
-    }
+    handleOtherDelemeter(number);
+  } else {
+    commaAndNewLineDelemeter(number);
   }
 }
 
+function commaAndNewLineDelemeter(number) {
+  let negativeNumbers = [];
+  let sum = 0;
+  if (number) {
+    let num = number.split(/[,\n]/);
+    for (let value of num) {
+      let val = Number(value);
+      if (val < 0) {
+        negativeNumbers.push(val);
+      } else {
+        sum = sum + val;
+      }
+    }
+    console.log('sum', sum);
+  }
 
-// add('1,5');
-// add('1\n2,3');
-// add('//[]\n1;[]2')
-add('//[***]\n1***3***3');
+  if (negativeNumbers.length > 0) {
+    console.log('negative numbers not allowed', negativeNumbers.join(','));
+    throw ('negative numbers not allowed', negativeNumbers.join(','));
+  }
+}
+
+function handleOtherDelemeter(number) {
+  const delimiterEndIndex = number.indexOf('\n');
+  let delimiter = number.substring(2, delimiterEndIndex);
+  if (delimiter.startsWith('[') && delimiter.endsWith(']')) {
+    delimiter = delimiter.slice(1, -1);
+  }
+  const numbersPart = number.substring(delimiterEndIndex + 1);
+  const modifiedNumbers = numbersPart.split(delimiter).join(',');
+  commaAndNewLineDelemeter(modifiedNumbers);
+}
+
+add('1,5');
+add('1\n2,3');
+add('//;\n1;2');
